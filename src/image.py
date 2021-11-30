b9da6fc390f68a1bafb5f8f0839c4d18d4ac1b13
gaguaoussama <94971019+gaguaoussama@users.noreply.github.com>
2021-11-30 11:56:18 +0100

    Add files via upload

--- src/image.py
+++ src/image.py
@@ -47,7 +47,22 @@ class Image:
     #   on retourne une nouvelle image binarisee
     #==============================================================================
     def binarisation(self, S):
-        pass
+        #Image vide créee
+        im_2=Image() 
+        #Initialisation d'un tableau de pixels de même taille que self 
+        #Utilisation d'une matrice zero(cases nulles) , uint8 :type unsigned int de 8 bytes 
+        im_2.set_pixels(np.zeros((self.H,self.W), dtype=np.uint8))
+        #Parcours des lignes:i et des colonnes: j
+        #Et Affectation de nouvelles valeurs a notre nouvelle image en fonction des pixels et du seuil
+        for i in range(self.H):
+            for j in range (self.W):
+               if (self.pixels[i][j]<S):
+                  im_2.pixels[i][j]=0
+               else:
+                  im_2.pixels[i][j]=255
+        return im_2
+                   
+                
 
 
     #==============================================================================
@@ -59,18 +74,60 @@ class Image:
     #   on retourne une nouvelle image recadree
     #==============================================================================
     def localisation(self):
-        pass
+        l_min=self.H
+        l_max=0
+        c_min=self.W
+        c_max=0
+        #Parcours de l'image en fixant les lignes et en parcourant les colonnes
+        #Cadrons l'image par suppression des lignes et des colonnes toutes blanches
+        for i in range(self.H):
+          for j in range (self.W):
+            if self.pixels[i][j]==0:
+                if j<c_min:
+                    c_min=j
+                if j>c_max:
+                    c_max=j
+                if i<l_min:
+                    l_min=i
+                if i>l_max:
+                    l_max=i
+        new_image=Image()
+        #Affectation / supsets sauvegardant l'image 
+        #Cadrage
+        new_image.set_pixels(self.pixels[l_min:l_max,c_min:c_max])
+        return new_image
+              
 
     #==============================================================================
     # Methode de redimensionnement d'image
     #==============================================================================
     def resize(self, new_H, new_W):
-        pass
+        #Creation d'une nouvelle image
+        new_image=Image()
+        #Redimensionnement et multiplication par 255 pour convertir en int
+        k=resize(self.pixels,(new_H,new_W),0)
+        new_image.set_pixels(np.uint8(k*255))
+        return new_image
+    
+        
 
 
     #==============================================================================
     # Methode de mesure de similitude entre l'image self et un modele im
     #==============================================================================
     def similitude(self, im):
-        pass
+        #Initialisation
+        x=0
+        #Parcours des colonnes en fixant les lignes
+        #Comparaisons de l'intensité de chaque 2 pixels de ligne 'i'et de colonne 'j'
+        for i in range(self.H):
+            for j in range(self.W):
+                if self.pixels[i][j]==im.pixels[i][j]:
+                    x+=1
+                else:
+                    x+=0
+        return(x/(self.H*self.W))
+                
+                
+       
 
